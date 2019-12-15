872e41f163d8b5e46e96e077ba46430579ea585e
h <besqer996@gnu.ac.kr>
2019-12-16 02:05:23 +0900

    Add dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import find_packages, setup
 
-
 author = {
     'name': 'Ho Kim',
     'email': 'ho.kim@gnu.ac.kr',
@@ -8,22 +7,35 @@ author = {
 
 
 setup(
-    name='test-macro',
     version='0.1',
+
+    name='test-macro',
+    description='a test automating library written in Python',
+    url=r'https://github.com/kerryeon/test-macro',
+
     author=author['name'],
     author_email=author['email'],
     maintainer=author['name'],
     maintainer_email=author['email'],
-    url=r'https://github.com/kerryeon/test-macro',
     license='MIT',
-    description='a test automating library written in Python',
-    platforms=[
 
-    ],
     packages=find_packages(),
     entry_points = {
         'console_scripts': [
             'macro=test_macro.macro:main',
         ],
     },
+
+    install_requires=[
+        'ewmh',
+        'lark-parser',
+        'matplotlib',
+        'mss',
+        'opencv-python',
+        'python-xlib',
+        'PyYAML',
+        'scipy',
+        'tqdm',
+    ],
+    zip_safe=False,
 )
